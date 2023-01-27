507de72cafa2fa5a2e65708d6885831eade6a2ec
villaclara <villaclara1231@gmail.com>
2023-01-27 14:35:23 +0200

    Add project files.

---
+++ ATA_Console/Program.cs
@@ -0,0 +1,18 @@
+﻿using ATA_WPF;
+using System;
+using System.Diagnostics;
+
+
+namespace ATA_Console
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            Class1 classbb = new Class1();
+            
+        }
+    }
+}
